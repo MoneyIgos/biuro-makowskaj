a75f18ca174022352cd7711ae7599a7ef5ef533d
MoneyIgos <makowskiigor@gmail.com>
2020-12-22 11:56:31 +0100

    feat: add header

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-main>
-    <v-container> Hello World </v-container>
-  </v-main>
+  <v-app dark>
+    <v-container>
+      <v-stepper-header class="d-flex flex-column justify-center">
+        <h1>Biuro Rachunkowe Joanna Makowska</h1>
+        <p>Kompleksowa obsługa księgowa dla firm i osób prywatnych</p>
+      </v-stepper-header>
+    </v-container>
+  </v-app>
 </template>
