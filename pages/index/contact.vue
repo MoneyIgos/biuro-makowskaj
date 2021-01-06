632312d3b3c33928205ec299eb9873c4d84ba83f
MoneyIgos <makowskiigor@gmail.com>
2021-01-06 12:38:52 +0100

    fix: delete double scroll bar

--- pages/index/contact.vue
+++ pages/index/contact.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-container>
-    <v-content>
-      <header><h1 class="text-uppercase">Kontakt</h1></header>
-    </v-content>
-  </v-container>
+  <v-content>
+    <header><h1 class="text-uppercase">Kontakt</h1></header>
+  </v-content>
 </template>
