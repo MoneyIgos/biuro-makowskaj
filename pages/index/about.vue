a4c42a74ba279dfe9bfe7c6c09968235dcc680be
MoneyIgos <makowskiigor@gmail.com>
2021-01-29 11:12:30 +0100

    fix: font-size and text align

--- pages/index/about.vue
+++ pages/index/about.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="d-flex flex-column align-center">
+  <div class="d-flex justify-center" style="height: 100%">
     <v-row>
-      <v-col>
+      <v-col md="4"></v-col>
+      <v-col class="about">
         <h1 class="text-uppercase align-self-start m-8">
           Witam na mojej stronie
         </h1>
@@ -9,10 +10,6 @@
           Proponuję Państwu profesjonalne usługi księgowe<br />
           w atrakcyjnych cenach z możliwością odbioru dokumentów od klienta.
         </p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
         <ul>
           <p>Posiadam:</p>
           <li>wieloletnie doświadczenie w obsłudze księgowej firm</li>
@@ -23,3 +20,9 @@
     </v-row>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  font-size: 1.5em;
+}
+</style>
